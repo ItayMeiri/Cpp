e7c03e2e3341267185bd27df7abe85b29d73e7c9
Itay Meiri <72934523+ItayMeiri@users.noreply.github.com>
2021-03-09 18:56:11 +0200

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -25,23 +25,24 @@ string nospaces(string input) {
 }
 
 
-TEST_CASE("Good snowman code") {
+TEST_CASE("Good snowman code") { // 
+
 	//HNLRXYTB
     CHECK(nospaces(snowman(11114411)) == nospaces("_===_\n(.,.)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(11111111)) == nospaces("_===_\n(.,.)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(12222222)) == nospaces("_===_\n(o.o)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(12341234)) == nospaces("_===_\n(O..)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(22222222)) == nospaces(" ___ .....\n(o.o)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(23333333)) == nospaces(" ___ .....\n(O,O)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(23412341)) == nospaces(" ___ .....\n(-,.)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(24123123)) == nospaces(" ___ .....\n(.,o)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(24444442)) == nospaces(" ___ .....\n(-,.)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(33333333)) == nospaces("_===_\n(O,O)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(34444441)) == nospaces("_===_\n(-,-)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(34444442)) == nospaces("_===_\n(-,-)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(34444443)) == nospaces("_===_\n(-,-)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(43214321)) == nospaces("_===_\n(o,.)\n( : )\n( : )"));
-	CHECK(nospaces(snowman(44444444)) == nospaces("_===_\n(-,-)\n( : )\n( : )"));
+	CHECK(nospaces(snowman(11111111)) == nospaces("_===_\n(.,.)\n<( : )>\n( : )"));
+	CHECK(nospaces(snowman(12222222)) == nospaces("_===_\n\\(o.o)/\n(] [)\n(\" \")"));
+	CHECK(nospaces(snowman(12341234)) == nospaces("_===_\n(O.-)/\n<(> <)\n(   )"));
+	CHECK(nospaces(snowman(22222222)) == nospaces(" ___ .....\n\\(o.o)/\n(] [)\n(\" \")"));
+	CHECK(nospaces(snowman(23333333)) == nospaces(" ___ .....\n(O_O)\n/(> <)\\\n(___)"));
+	CHECK(nospaces(snowman(23412341)) == nospaces(" ___ .....\n\\(-_.)\n(   )\\\n( : )"));
+	CHECK(nospaces(snowman(24123123)) == nospaces(" ___ .....\n(.o)\n/(] [)>\n(___)"));
+	CHECK(nospaces(snowman(24444442)) == nospaces(" ___ .....\n(--)\n(   )\n(\" \")"));
+	CHECK(nospaces(snowman(33333333)) == nospaces("   _\n  /_\\\n(O_O)\n/(> <)\\\n(___)"));
+	CHECK(nospaces(snowman(34444441)) == nospaces("   _\n  /_\\\n(--)\n(   )\n( : )"));
+	CHECK(nospaces(snowman(34444442)) == nospaces("   _\n  /_\\\n(--)\n(   )\n(\" \")"));
+	CHECK(nospaces(snowman(34444443)) == nospaces("   _\n  /_\\\n(--)\n(   )\n(___)"));
+	CHECK(nospaces(snowman(43214321)) == nospaces("  ___ \n (_*_)\n(o_.)\n(] [)\\\n( : )"));
+	CHECK(nospaces(snowman(44444444)) == nospaces("  ___ \n (_*_)\n(--)\n(   )\n(   )"));
 
 }
 
--- snowman.cpp
+++ snowman.cpp
@@ -1,11 +1,8 @@
 #include "snowman.hpp"
 #include <iostream>
 
-
-
 namespace ariel
 {
-
 std::array<std::string, 4> hats = {
     " _===_",
     "  ___\n .....",
@@ -19,10 +16,8 @@ std::array<std::string, 4> torso = {" : ", "] [", "> <", "   "};
 std::array<std::string, 4> leftHand = {" <", "\\ ", " /", "  "};
 std::array<std::string, 4> rightHand = {" >", "/ ", " \\", "  "};
 
-
 std::string snowman(int seed)
 {
-
     // validate snowman seed, throws exception when invalid
     std::array<std::string, 8> strSeed = seedValidator(seed); // stored as the seed HNLRXYTB 
 
@@ -35,7 +30,6 @@ std::string snowman(int seed)
     +strSeed.at(6) + ")" // torso
     +strSeed.at(5).at(1) + "\n (" // left arm, char 1
     +strSeed.at(7) + ")";
-
 
     return sman;
 }
@@ -50,22 +44,20 @@ std::array<std::string, 8> seedValidator(int seed)
         if (seed % 10 > 4){ // get last digit
             throw std::invalid_argument("Invalid code '5'");
         }
-        int index = seed % 10;
-        index--;
-         if(count == 7){str.at(0) = hats.at(index);}
-         else if(count == 6){str.at(1) = noses.at(index);}
-         else if(count == 5){str.at(2) = eyes.at(index);} // left
-         else if(count == 4){str.at(3) = eyes.at(index);} // right
-         else if(count == 3){str.at(4) = leftHand.at(index);}
-         else if(count == 2){str.at(5) = rightHand.at(index);}
-         else if(count == 1){str.at(6) = torso.at(index);}
-         else if(count == 0){str.at(7) = base.at(index);}
+        int index = (seed % 10) - 1;
+        if(count == 7){str.at(0) = hats.at(index);}
+        else if(count == 6){str.at(1) = noses.at(index);}
+        else if(count == 5){str.at(2) = eyes.at(index);} // left
+        else if(count == 4){str.at(3) = eyes.at(index);} // right
+        else if(count == 3){str.at(4) = leftHand.at(index);}
+        else if(count == 2){str.at(5) = rightHand.at(index);}
+        else if(count == 1){str.at(6) = torso.at(index);}
+        else if(count == 0){str.at(7) = base.at(index);}
 
-        seed = seed/10; // removes digit
+        seed = seed/10; // removes last digit
         count++; // counts the digits
     }
-    if(count != 8)
-    {
+    if(count != 8){
         throw std::invalid_argument("Invalid code '5'");
     }
     return str;
